dc22cccd3bd687efd02be04d00a9d0628c525650
subhajitpal1993 <34128456+subhajitpal1993@users.noreply.github.com>
2021-11-11 18:38:54 +0530

    Add files via upload

---
+++ shapash/tests/integration_tests/test_contributions_multiclass.py
@@ -0,0 +1,110 @@
+"""
+Unit test contributions multiclass
+"""
+import unittest
+import shap
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+from shapash.decomposition.contributions import rank_contributions
+from shapash.utils.shap_backend import shap_contributions
+
+
+class TestContributions(unittest.TestCase):
+    """
+    Unit test Contributions Class
+    TODO: Docstring
+    Parameters
+    ----------
+    unittest : [type]
+        [description]
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    def setUp(self):
+        """
+        Setup
+        """
+        iris = load_iris()
+        x_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+        y_df = pd.DataFrame(data=iris.target, columns=["target"])
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            x_df,
+            y_df,
+            random_state=1
+        )
+
+    def check_compute_contributions(self, slist, blist, x_test):
+        """
+        Unit test compute contributions
+        Parameters
+        ----------
+        slist : list
+            [description]
+        blist : list
+            [description]
+        x_test : pandas.DataFrame
+            [description]
+        """
+        assert len(slist) == 3
+        assert len(blist) == 3
+        for i in range(3):
+            assert slist[i].shape == x_test.shape
+            assert slist[i].index.equals(x_test.index)
+            np.testing.assert_array_equal(np.array(x_test.columns), np.array(x_test.columns))
+            assert slist[i].isnull().sum().sum() == 0
+
+    def check_sum_contributions(self, slist, blist, y_dataframe):
+        """
+        Unit test check sum contributions
+        Parameters
+        ----------
+        slist : list
+            [description]
+        blist : list
+            [description]
+        y_dataframe : [type]
+            [description]
+        """
+        for i in range(3):
+            np.testing.assert_almost_equal(slist[i].sum(axis=1) + blist[i], y_dataframe[:, i], decimal=5)
+
+    def get_predictions(self, model, **args):
+        """
+        Method to get predictions
+        Parameters
+        ----------
+        model : [type]
+            [description]
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        model.fit(self.x_train, self.y_train)
+        if args:
+            return model.predict(self.x_test, **args)
+        else:
+            return model.predict(self.x_test)
+
+    def test_rank_contributions_1(self):
+        """
+        Unit test rank contributions 1
+        """
+        model = RandomForestClassifier(n_estimators=3)
+        model.fit(self.x_train, self.y_train)
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(self.x_test)
+        slist = [pd.DataFrame(data=tab, index=self.x_test.index, columns=self.x_test.columns) for tab in shap_values]
+
+        for i in range(3):
+            s_ord, x_ord, s_dict = rank_contributions(slist[i], pd.DataFrame(data=self.x_test))
+            assert np.all(np.diff(np.abs(s_ord), axis=1) <= 0) == 1
+            assert np.array_equal(
+                x_ord.values,
+                np.take_along_axis(self.x_test.values, s_dict.values, axis=1)
+            )
---
+++ shapash/tests/unit_tests/decomposition/test_contributions.py
@@ -0,0 +1,71 @@
+"""
+Unit test of contributions
+"""
+import unittest
+import pandas as pd
+import numpy as np
+from shapash.decomposition.contributions import rank_contributions
+
+    
+class TestContributions(unittest.TestCase):
+    """
+    Unit test class of contributions
+    TODO: Docstring
+    Parameters
+    ----------
+    unittest : [type]
+        [description]
+    """
+
+    def test_rank_contributions_1(self):
+        """
+        Unit test rank contributions 1
+        """
+        dataframe_s = pd.DataFrame(
+            [[3.4, 1, -9, 4],
+             [-45, 3, 43, -9]],
+            columns=["Phi_" + str(i) for i in range(4)],
+            index=['raw_1', 'raw_2']
+        )
+
+        dataframe_x = pd.DataFrame(
+            [['Male', 'House', 'Married', 'PhD'],
+             ['Female', 'Flat', 'Married', 'Master']],
+            columns=["X" + str(i) for i in range(4)],
+            index=['raw_1', 'raw_2']
+        )
+
+        expected_s_ord = pd.DataFrame(
+            data=[[-9, 4, 3.4, 1],
+                  [-45, 43, -9, 3]],
+            columns=['contribution_' + str(i) for i in range(4)],
+            index=['raw_1', 'raw_2']
+        )
+
+        expected_x_ord = pd.DataFrame(
+            data=[['Married', 'PhD', 'Male', 'House'],
+                  ['Female', 'Married', 'Master', 'Flat']],
+            columns=['feature_' + str(i) for i in range(4)],
+            index=['raw_1', 'raw_2']
+        )
+
+        expected_s_dict = pd.DataFrame(
+            data=[[2, 3, 0, 1],
+                  [0, 2, 3, 1]],
+            columns=['feature_' + str(i) for i in range(4)],
+            index=['raw_1', 'raw_2']
+        )
+
+        s_ord, x_ord, s_dict = rank_contributions(dataframe_s, dataframe_x)
+
+        assert np.array_equal(s_ord.values, expected_s_ord.values)
+        assert np.array_equal(x_ord.values, expected_x_ord.values)
+        assert np.array_equal(s_dict.values, expected_s_dict.values)
+
+        assert list(s_ord.columns) == list(expected_s_ord.columns)
+        assert list(x_ord.columns) == list(expected_x_ord.columns)
+        assert list(s_dict.columns) == list(expected_s_dict.columns)
+
+        assert pd.Index.equals(s_ord.index, expected_s_ord.index)
+        assert pd.Index.equals(x_ord.index, expected_x_ord.index)
+        assert pd.Index.equals(s_dict.index, expected_s_dict.index)
